fd40cc53ee1d55dce9ac4f814c162a17dbad5010
aryanrajputdesign <6dfvmdrh46@privaterelay.appleid.com>
2026-07-10 20:38:00 +0500

    Update script.js

--- script.js
+++ script.js
@@ -27,4 +27,31 @@ document.getElementById("loader").classList.add("hide");
 
 },1200);
 
+
+});
+
+// Animated Counters
+
+const counters = document.querySelectorAll(".counter");
+
+counters.forEach(counter => {
+
+    const updateCounter = () => {
+
+        const target = +counter.getAttribute("data-target");
+        const count = +counter.innerText;
+
+        const increment = target / 100;
+
+        if (count < target) {
+            counter.innerText = Math.ceil(count + increment);
+            setTimeout(updateCounter, 20);
+        } else {
+            counter.innerText = target + "+";
+        }
+
+    };
+
+    updateCounter();
+
 });
