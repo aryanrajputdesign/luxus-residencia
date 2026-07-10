133306244ee6442fbe758e386cdc7389c4a32860
aryanrajputdesign <6dfvmdrh46@privaterelay.appleid.com>
2026-07-10 06:29:20 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 console.log("Luxus Residencia Website Loaded");
+
+window.addEventListener("scroll", function () {
+
+const header = document.querySelector("header");
+
+if (window.scrollY > 80) {
+    header.classList.add("scrolled");
+} else {
+    header.classList.remove("scrolled");
+}
+
+});
