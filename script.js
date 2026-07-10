351fcc850128f47d554061ff5beb5b197fd26193
aryanrajputdesign <6dfvmdrh46@privaterelay.appleid.com>
2026-07-10 14:35:29 +0500

    Update script.js

--- script.js
+++ script.js
@@ -11,3 +11,10 @@ if (window.scrollY > 80) {
 }
 
 });
+
+const menu = document.querySelector(".menu-toggle");
+const nav = document.querySelector("nav");
+
+menu.addEventListener("click", function () {
+nav.classList.toggle("active");
+});
