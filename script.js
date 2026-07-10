7da18d7a21051ec7c63b2b4b6f55ba1b8890e5ee
aryanrajputdesign <6dfvmdrh46@privaterelay.appleid.com>
2026-07-11 03:54:21 +0500

    Update script.js

--- script.js
+++ script.js
@@ -56,23 +56,7 @@ counters.forEach(counter => {
 
 });
 
-const slides = document.querySelectorAll(".slide");
 
-let currentSlide = 0;
-
-if (slides.length > 0) {
-
-setInterval(() => {
-
-slides[currentSlide].classList.remove("active");
-
-currentSlide = (currentSlide + 1) % slides.length;
-
-slides[currentSlide].classList.add("active");
-
-}, 5000);
-
-}
 
 const slides = document.querySelectorAll(".slide");
 const dots = document.querySelectorAll(".dot");
