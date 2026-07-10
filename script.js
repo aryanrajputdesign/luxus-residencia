31c3efb7341a19c81d2e472914c3a3bbd5c5f07c
aryanrajputdesign <6dfvmdrh46@privaterelay.appleid.com>
2026-07-11 03:21:11 +0500

    Update script.js

--- script.js
+++ script.js
@@ -55,3 +55,21 @@ counters.forEach(counter => {
     updateCounter();
 
 });
+
+const slides = document.querySelectorAll(".slide");
+
+let currentSlide = 0;
+
+if (slides.length > 0) {
+
+setInterval(() => {
+
+slides[currentSlide].classList.remove("active");
+
+currentSlide = (currentSlide + 1) % slides.length;
+
+slides[currentSlide].classList.add("active");
+
+}, 5000);
+
+}
