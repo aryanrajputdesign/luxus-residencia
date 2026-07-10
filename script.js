53e6e378f00819433dbd75aecd74cee93a4e7003
aryanrajputdesign <6dfvmdrh46@privaterelay.appleid.com>
2026-07-11 03:48:20 +0500

    Update script.js

--- script.js
+++ script.js
@@ -73,3 +73,45 @@ slides[currentSlide].classList.add("active");
 }, 5000);
 
 }
+
+const slides = document.querySelectorAll(".slide");
+const dots = document.querySelectorAll(".dot");
+const prevBtn = document.getElementById("prevSlide");
+const nextBtn = document.getElementById("nextSlide");
+
+let currentSlide = 0;
+
+function showSlide(index){
+    slides.forEach(slide => slide.classList.remove("active"));
+    dots.forEach(dot => dot.classList.remove("active"));
+
+    slides[index].classList.add("active");
+    dots[index].classList.add("active");
+}
+
+function nextSlide(){
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
+}
+
+function prevSlide(){
+    currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+    showSlide(currentSlide);
+}
+
+if(nextBtn){
+    nextBtn.addEventListener("click", nextSlide);
+}
+
+if(prevBtn){
+    prevBtn.addEventListener("click", prevSlide);
+}
+
+dots.forEach((dot,index)=>{
+    dot.addEventListener("click",()=>{
+        currentSlide=index;
+        showSlide(currentSlide);
+    });
+});
+
+setInterval(nextSlide,5000);
